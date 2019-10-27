33a54407ec55853237ddabc9c4a7938e4fcae346
Swiss <wankhedepawan1996@gmail.com>
2019-10-28 02:59:35 +0530

    page 20

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/page-14-1.2.c
@@ -0,0 +1,13 @@
+/* concrete.c */
+int main(void)
+{
+	printf("Concrete contains gravel and cement.\n");
+
+	return 0;
+}
+
+
+/* gcc Command variations for different c standards from page 18 */
+/* gcc -std=c99 filename.c
+ * gcc -std=clx filename.c
+ * gcc -std=c11 filename.c*/
