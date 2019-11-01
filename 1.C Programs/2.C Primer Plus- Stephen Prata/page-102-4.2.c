c5aca2bec5b6056d13d29e32e35ef433e02e048a
Swiss <wankhedepawan1996@gmail.com>
2019-11-02 01:52:24 +0530

    reached page 120

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/page-102-4.2.c
@@ -0,0 +1,14 @@
+// uses an assortment of strings
+#include<stdio.h>
+#define PRAISE "You are an extraordinary being."
+
+int main(void)
+{
+	char name[40];
+
+	printf("What's your name? ");
+	scanf("%s", name);
+	printf("Hello, %s, %s, %s\n", name, PRAISE);
+
+	return 0;
+}
