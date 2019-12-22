8750fb1643c5c5c46857bd47037cb5544807c163
Swiss <wankhedepawan1996@gmail.com>
2019-12-22 10:45:20 +0530

    reached page 439

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-10/11.c
@@ -0,0 +1,45 @@
+// Que 11.c
+
+#include <stdio.h>
+
+void print_Nx5_int_array(int (*array)[5], int rows);
+void double_Nx5_int_array(int (*array)[5], int rows);
+
+void print_Nx5_int_array(int (*array)[5], int rows)
+{
+	// print the contents of an N x 5 array of ints
+	
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			printf(" %5d ", array[i][j]);
+		}
+
+		putchar('\n');
+	}
+}
+
+void double_Nx5_int_array(int (*array)[5], int rows)
+{
+	// double the elements of an N x 5 array of ints
+	
+	for (int i = 0; i < rows; i++)
+		for (int j = 0; j < 5; j++)
+			array[i][j] *= 2;
+}	
+
+int main(void)
+{
+	int array[3][5] = {{ 2, 5, 6, 9, 1}, {23, 1, 5, 66, 54}, {9, 73, 23, 39, 2}};
+
+	printf("Original array:\n");
+	print_Nx5_int_array(array, 3);
+
+	// double array and print new values
+	double_Nx5_int_array(array, 3);
+	printf("Doubled array:\n");
+	print_Nx5_int_array(array, 3);
+
+	return 0;
+}
---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-10/8.c
@@ -0,0 +1,33 @@
+// Que 8.c
+
+#include <stdio.h>
+
+void copy_ptrs(double *target, double *source_start, double *source_end);
+
+void copy_ptrs(double *target, double *source_start, double *source_end)
+{
+	//copy array using pointer notation and pointer endpoint
+	
+	for (double *ptr = source_start;  ptr < source_end; ptr++, target++)
+		*target = *ptr;
+}
+
+int main(void)
+{
+	double source[7] = {2.4, 5.9, 7.8, 1.5, 3.3, 5.3, 6.8};
+	double target[3];
+
+	copy_ptrs(target, source + 2, source + 5);
+
+	// print arrays
+	for (int i= 0; i < 7; i++)
+		printf("%.1f ", source[i]);
+	putchar('\n');
+
+	for (int i = 0; i < 3; i++)
+		printf("%.1f ", target[i]);
+	putchar('\n');
+
+
+	return 0;
+}
