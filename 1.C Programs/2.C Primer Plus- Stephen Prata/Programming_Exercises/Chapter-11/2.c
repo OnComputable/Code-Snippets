f1323f52c8906bc1910b2bc01863059ddd33e152
Swiss <wankhedepawan1996@gmail.com>
2019-12-30 11:34:43 +0530

    reached que 5

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-11/2.c
@@ -0,0 +1,40 @@
+// Que 2.c
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define SIZE 20
+
+char * sgetnchar(char *array, int n);
+
+char * sgetnchar(char *array, int n)
+{
+	// get characters from input and store them in character array
+	// stopping after n characters or first whitespace
+	
+	int i = 0;
+	char ch;
+
+	while ((ch = getchar()) != EOF && !isspace(ch) && i < n)
+	{
+		*(array + i) = ch;
+		i++;
+	}
+
+	return array;
+}
+
+int main(void)
+{
+	// test sgetnchar
+	
+	char hello[SIZE] = "Hello, ";
+	int space = SIZE - strlen(hello) - 1;
+
+	puts("What's your name?");
+	sgetnchar(hello + 7, space);
+	puts(hello);
+
+	return 0;
+}
---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-11/5.c
@@ -0,0 +1,56 @@
+// Que 5.c
+
+#include <stdio.h>
+#define LIMIT 50
+
+char * findchar(char *str, const char ch);
+
+char * findchar(char *str, const char ch)
+{
+	// find and return pointer to first occurece of
+	// char ch in string str. return NULL if not found
+	
+	while (*str != '\0')
+	{
+		if (*str == ch)
+			return str;
+		str++;
+	}
+
+	// if ch is not found, return NULL
+	return NULL;
+}
+
+int main(void)
+{
+	char string[LIMIT];
+	char *chloc;
+	char ch;
+
+	// test findchar()
+	printf("Enter a string to search: ");
+	fgets(string, LIMIT, stdin);
+	while (string[0] != '\n')
+	{
+		printf("Enter a character to search for: ");
+		ch = getchar();
+		// discard rest of line if any
+		if (ch != '\n')
+			while (getchar() != '\n')
+				continue;
+
+		chloc = findchar(string, ch);
+		if (chloc == NULL)
+			printf("Character %c not found in %s", ch, string);
+		else
+			printf("Character %c found at index %lu in %s", ch, chloc - string, string );
+
+		// get new input
+		printf("Enter a string to search (empty line to quit): ");
+		fgets(string, LIMIT, stdin);
+	}
+
+	puts("Bye");
+
+	return 0;
+}
