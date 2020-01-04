ae280d6c15d20c9ead692732440030d57e40e329
Swiss <wankhedepawan1996@gmail.com>
2020-01-04 05:50:36 +0530

    reached page 540

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/page-532-12.5-c/1.c
@@ -0,0 +1,33 @@
+// prata.c -- various storage classes
+// compile with 2.c
+
+#include <stdio.h>
+
+void report_count();
+void accumulate(int k);
+
+
+int count = 0;	// file scope, external linkage
+
+void report_count()
+{
+	printf("Loop executed %d times\n", count);
+}
+
+int main(void)
+{
+	int value;	// automatic variable
+	register int i;	// register variable
+
+	printf("Enter a positive integer (0 to quit): ");
+	while (scanf("%d", &value) == 1 && value > 0)
+	{
+		++count;	// use file scope variable
+		for (i = value; i >= 0; i--)
+			accumulate(i);
+		printf("Enter a positive integer (0 to quit): ");
+	}
+	report_count();
+
+	return 0;
+}
