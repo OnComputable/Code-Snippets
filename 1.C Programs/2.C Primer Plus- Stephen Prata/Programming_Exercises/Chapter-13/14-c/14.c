52c00314458026f078c15e91d20d3340ea9ebbbe
Swiss <wankhedepawan1996@gmail.com>
2020-01-15 16:14:55 +0530

    reached page 600

--- 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-13/14-c/14.c
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-13/14-c/14.c
@@ -10,7 +10,51 @@ void deglitch(int data[ROWS][COLS]);
 
 void deglitch(int data[ROWS][COLS])
 {
-	
+	float total;
+	int count;
+
+	for (int i = 0; i < ROWS; i++)
+		for (int j = 0; j < COLS; j++)	
+		{
+			total = 0;
+			count = 0;
+			if (i + 1 < ROWS)
+			{
+				if (abs(data[i][j] - data[i + 1][j]) < 2)
+					continue;
+
+				total += data[i + 1][j];
+				count++;
+			}
+			if (j + 1 < COLS)
+			{
+				if (abs(data[i][j] - data[i][j + 1]) < 2)
+					continue;
+
+				total += data[i][j + 1];
+				count++;
+			}
+			if (i > 0)
+			{
+				if (abs(data[i][j] - data[i - 1][j]) < 2)
+					continue;
+
+				total += data[i - 1][j];
+				count++;
+			}
+			if (j > 0)
+			{
+				if (abs(data[i][j] - data[i][j - 1]) < 2)
+					continue;
+
+				total += data[i][j - 1];
+				count++;
+			}	
+
+			// if none of the continue statements have been triggered, 
+			// replace data[i][j] with the rounded average of it's neighbours
+			data[i][j] = (int) rintf(total / count);
+		}
 }
 
 int main(int argc, char *argv[])
@@ -45,6 +89,68 @@ int main(int argc, char *argv[])
 
 	// de-glitch the data
 	deglitch(data);
+
+	// convert ints to characters
+	for (int i = 0; i < ROWS; i++)
+	{
+		int j;
+		for (j = 0; j < COLS; j++)
+		{
+			switch (data[i][j])
+			{
+				case(0):
+					ch = ' ';
+					break;
+				case(1):
+					ch = '.';
+					break;
+				case(2):
+					ch = '\'';
+					break;
+				case(3):
+					ch = ':';
+					break;
+				case(4):
+					ch = '~';
+					break;
+				case(5):
+					ch = '*';
+					break;
+				case(6):
+					ch = '=';
+					break;
+				case(7):
+					ch = '}';
+					break;
+				case(8):
+					ch = '&';
+					break;
+				case(9):
+					ch = '#';
+					break;
+				default:
+					fprintf(stderr, "Error: int out of bounds.\n");
+					exit(EXIT_FAILURE);
+			}
+			img[i][j] = ch;
+		}
+		img[i][j] = '\0';	// j = COLS here
+	}
+
+	// open image file
+	if ((fp = fopen(argv[2], "w")) == NULL)
+	{
+		fprintf(stderr, "Could not open file %s.\n", argv[2]);
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < ROWS; i++)
+	{
+		// print to console and file
+		puts(img[i]);
+		fprintf(fp, "%s\n", img[i]);
+	}
+	fclose(fp);
 
 	return 0;
 }
