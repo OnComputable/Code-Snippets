f992a8850e7aef8d7b6615b03ddc6fd730ed1705
Swiss <wankhedepawan1996@gmail.com>
2020-02-17 14:42:23 +0530

    On branch master

--- 1.C Programs/2.C Primer Plus- Stephen Prata/Chapter-17-Advanced_Data_Representation/page-830-17.10-tree/petclub.c
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Chapter-17-Advanced_Data_Representation/page-830-17.10-tree/petclub.c
@@ -55,6 +55,93 @@ void addpet(Tree * pt)
 	}
 }
 
+void showpets(const Tree * pt)
+{
+	if (TreeIsEmpty(pt))
+		puts("No entries!");
+	else
+		Traverse(pt, printitem);
+}
+
+void printitem(Item item)
+{
+	printf("Pet: %-19s  Kind: %-19s\n", item.petname, item.petkind);
+}
+
+void findpet(const Tree * pt)
+{
+	Item temp;
+
+	if (TreeIsEmpty(pt))
+	{
+		puts("No entries!");
+		return;	// quit function if tree is empty
+	}
+
+	puts("Please enter name of pet you wish to find:");
+	s_gets(temp.petname, SLEN);
+	puts("Please enter pet kind:");
+	s_gets(temp.petkind, SLEN);
+	uppercase(temp.petname);
+	uppercase(temp.petkind);
+	printf("%s the %s ", temp.petname, temp.petkind);
+	if (InTree(&temo, pt))
+		printf("is a member.\n");
+	else
+		printf("is not a member.\n");
+}
+
+void droppet(Tree * pt)
+{
+	Item temp;
+
+	if (TreeIsEmpty(pt))
+	{
+		puts("No entries!");
+		return;		// quit function if tree is empty
+	}
+
+	puts("Please enter name of pet you wish to delete:");
+	s_gets(temp.petname, SLEN);
+	puts("Please enter pet kind:");
+	s_gets(temp.petkind, SLEN);
+	uppercase(temp.petname);
+	uppercase(temp.petkind);
+	printf("%s the %s ", temp.petname, temp.petkind);
+	if (DeleteItem(&temp, pt))
+		printf("is dropped from the club.\n");
+	else
+		printf("is not a member.\n");
+}
+
+void uppercase(char * str)
+{
+	while (*str)
+	{
+		*str = toupper(*str);
+		str++;
+	}
+}
+
+
+char * s_gets(char * st, int n)
+{
+	char * ret_val;
+	char * find;
+
+	ret_val = fgets(st, n, stdin);
+	if (ret_val)
+	{
+		find = strchr(st, '\n');	// look for newline
+		if (find)			// if the address is not NULL,
+			*find = '\0';		// place a null character there
+		else
+			while (getchar() != '\n')
+				continue;	// dispose of rest of line
+	}
+	return ret_val;
+}
+
 int main(void)
 {
 	Tree pets;
