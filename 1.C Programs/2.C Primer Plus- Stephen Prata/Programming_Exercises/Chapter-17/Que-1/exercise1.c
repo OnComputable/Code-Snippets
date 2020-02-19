54291b5e6436ed76f4c838e1fe97dc6e7c0fdb43
Swiss <wankhedepawan1996@gmail.com>
2020-02-19 23:26:19 +0530

    On branch master

---
+++ 1.C Programs/2.C Primer Plus- Stephen Prata/Programming_Exercises/Chapter-17/Que-1/exercise1.c
@@ -0,0 +1,71 @@
+// Que 1.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "film.h"
+
+char * get(char * string, int n);
+
+char * get(char * string, int n)
+{
+	// wrapper for fgets that replaces first newline with null
+	
+	char *return_value = fgets(string, n, stdin);
+
+	while (*string != '\0')
+	{
+		if (*string == '\n')
+		{
+			*string = '\0';
+			break;
+		}
+		string++;
+	}
+
+	return return_value;
+}
+
+int main(void)
+{
+	List film_list = NULL;
+	Film * current;
+	char input[TSIZE];
+
+	puts("Enter first movie title:");
+	while (get(input, TSIZE) != NULL && input[0] != '\0')
+	{
+		current = (Film *) malloc(sizeof(Film));
+		if (current == NULL)
+		{
+			fprintf(stderr, "Could not allocate memory.\n");
+			exit(EXIT_FAILURE);
+		}
+
+		current->next = NULL;
+		strcpy(current->title, input);
+		printf("Enter your rating (0 - 10): ");
+		scanf("%d", &(current->rating));
+		while (getchar() != '\n') continue;
+
+		if (film_list == NULL)
+			film_list = current;
+		else
+			add_film(film_list, current);
+		puts("Enter next movie title (empty line to stop):");
+	}
+
+	// show list of movies
+	puts("Here is your list of movies:");
+	list_films(film_list);
+	puts("");
+
+	// show list of movies in reverse order
+	puts("Here is your list of movies in reverse order:");
+	reverse_list_films(film_list);
+
+	// clean up
+	delete_list(&film_list);
+
+	return 0;
+}
