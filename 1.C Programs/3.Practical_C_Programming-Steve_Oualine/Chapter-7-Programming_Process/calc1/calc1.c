3292141b16027154d53afe26540d0221837281f2
Swiss <wankhedepawan1996@gmail.com>
2020-03-07 14:39:19 +0530

    reached page 110

---
+++ 1.C Programs/3.Practical_C_Programming-Steve_Oualine/Chapter-7-Programming_Process/calc1/calc1.c
@@ -0,0 +1,36 @@
+/********************************************************
+ * [ File: calc1/calc1.c ]
+ * ******************************************************/ 
+#include <stdio.h>
+char line[100];	// line of data from the input
+int result;	// the result of the calculations
+char operator;	// operator the user specified
+int value;	// value specified after the operator
+
+int main()
+{
+	result = 0;	// initialize the result
+
+	// Loop forever {or till we hit the break statement}
+	while (1)
+	{
+		printf("Result: %d\n", result);
+
+		printf("Enter operator and number: ");
+		fgets(line, sizeof(line), stdin);
+		sscanf(line, "%c %d", &operator, &value);
+		printf("## after scanf %c\n", operator);
+
+		if (operator == '+')
+		{	
+			printf("## after if %c\n", operator);
+			result += value;
+		}
+		else
+		{
+			printf("Unknown operator %c\n", operator);
+		}
+	}
+
+	return (0);
+}
