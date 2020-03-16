9888bd641b774a7fdfb8dfa93f912fb03cf19571
Swiss <wankhedepawan1996@gmail.com>
2020-03-16 18:09:02 +0530

    reached page 170

---
+++ 1.C Programs/3.Practical_C_Programming-Steve_Oualine/PART-2/Chapter-11-Bit_Oprations/page-166-11-2.c
@@ -0,0 +1,30 @@
+/********************************************************
+ * [ File: high/high.c ]
+ * ******************************************************/ 
+
+#include <stdio.h>
+
+const int HIGH_SPEED = (1<<7);	// modem is running fast
+
+// we are using a hardwired connection
+const int DIRECT_CONNECT = (1<<8);
+
+char flags = 0;		// start with nothing
+
+int main()
+{
+	flags |= HIGH_SPEED;	// we are running fast
+	flags |= DIRECT_CONNECT;	// because we are wired together
+
+	if ((flags & HIGH_SPEED) != 0)
+		printf("High Speed set\n");
+
+	if ((flags & DIRECT_CONNECT) != 0)
+		printf("Direct Connect set\n");
+
+	return (0);
+}
+
+/********************************************************
+ * Question 11-1: Answer: 
+ * ******************************************************/ 
